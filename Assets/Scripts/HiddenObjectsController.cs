d2fdd6c71bb5718bf9cf09c7e5fad140bdb0dece
lvlupJamie <aeboltt@gmail.com>
2026-08-23 13:00:22 +0200

    Hidden Objects code & hidden objects prefab with items added

---
+++ Assets/Scripts/HiddenObjectsController.cs
@@ -0,0 +1,23 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HiddenObjectsController : MonoBehaviour
+{
+    [SerializeField] GameObject hiddenObjectsPrefab;
+
+    public void OnCameraUp(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            ToggleObjects();
+            //moveSpeed = 3f;
+        }
+    }
+
+    void ToggleObjects()
+    {
+        bool currentState = hiddenObjectsPrefab.activeSelf;
+        hiddenObjectsPrefab.SetActive(!currentState);
+    }
+}
